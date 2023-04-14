47b09ba89169c7b439559adf03b5457c2e36aa64
John <95353217+FlyingThaCat@users.noreply.github.com>
2023-04-14 20:21:54 +0700

    Init Commit

---
+++ app.config.ts
@@ -0,0 +1,26 @@
+export default defineAppConfig({
+  docus: {
+    title: 'JTUDOCS',
+    description: 'All About My Understanding In My School.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      github: 'FlyingThaCat',
+      instagram: 'XI_TKJ_1_JOHN'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      title: "JTU DOCS"
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'simple-icons:nuxtdotjs'
+        }
+      ]
+    }
+  }
+})
