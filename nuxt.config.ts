1eb008e0679c252f03990033154149171fb7fe78
FlyingThaCat <95353217+FlyingThaCat@users.noreply.github.com>
2023-04-30 23:10:07 +0700

    Beta Build ...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,12 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
-})
-
-export default {
-  head: {
-    script: [
-      {
+  app: {
+    head: {
+      script: [{
         src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5130840829479969",
         async: true,
         crossorigin: "anonymous"
-      },
-    ],
+      }],
+    },
   },
-}
+  extends: '@nuxt-themes/docus'
+})
