af823e4eb879e124c5eca70701b32350fdd159d2
FlyingThaCat <95353217+FlyingThaCat@users.noreply.github.com>
2023-04-30 21:32:10 +0700

    Update nuxt.config.ts
    Testing Maybe

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,14 @@
-export default defineNuxtConfig({
+export default defineNuxtConfig(
+  head: {
+    script: [
+      {
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5130840829479969",
+        async: true,
+        crossorigin: "anonymous"
+      },
+    ],
+  },
+  {
   extends: '@nuxt-themes/docus'
-})
+  }
+)
