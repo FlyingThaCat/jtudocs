45d81ee18a445ad8e562cd2453f2967cd41a905f
flyingthacat <2021071031@student.pppkpetra.sch.id>
2023-05-19 09:48:54 +0700

    fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,12 @@
-export default defineNuxtConfig(
-  head: {
-    script: [
-      {
+export default defineNuxtConfig({
+  app: {
+    head: {
+      script: [{
         src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5130840829479969",
         async: true,
         crossorigin: "anonymous"
-      },
-    ],
+      }],
+    },
   },
-  {
   extends: '@nuxt-themes/docus'
 })
-
-// head: {
-  // script: [
-    // {
-      // src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5130840829479969",
-      // async: true,
-      // crossorigin: "anonymous"
-    // },
-  // ],
-// },
