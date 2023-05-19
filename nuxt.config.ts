007d14cd8d79ea6513c5647ac577814047fda3d6
flyingthacat <2021071031@student.pppkpetra.sch.id>
2023-05-19 09:42:32 +0700

    semoga ga -1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
-export default defineNuxtConfig({
-  app: {
-    head: {
-      script: [{
+export default defineNuxtConfig(
+  head: {
+    script: [
+      {
         src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5130840829479969",
         async: true,
         crossorigin: "anonymous"
-      }],
-    },
+      },
+    ],
   },
+  {
   extends: '@nuxt-themes/docus'
 })
+
+// head: {
+  // script: [
+    // {
+      // src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5130840829479969",
+      // async: true,
+      // crossorigin: "anonymous"
+    // },
+  // ],
+// },
